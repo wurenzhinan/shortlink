60383e35a2d6f3bf40ce307f5c0c92a9b78905a8
wurenzhinan <wurenzhinan@163.com>
2024-07-10 17:06:29 +0800

    feature:提交MySQL数据库分页插件

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/config/DataBaseConfiguration.java
@@ -0,0 +1,25 @@
+package com.nageoffer.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+
+ * 类描述： DataBaseConfiguration
+ **/
+@Configuration
+public class DataBaseConfiguration {
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
