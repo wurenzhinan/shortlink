fcddfa677754207d6bf2b7f8ac29df8ad2fa95c3
wurenzhinan <wurenzhinan@163.com>
2024-07-11 10:58:36 +0800

    optimize:优化短链接分组查询返回实体

--- admin/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
@@ -19,21 +19,18 @@ package com.nageoffer.shortlink.admin.test;
  * 类描述： UserTableShardingTest
  **/
 public class UserTableShardingTest {
-    public  static  final String SQL="CREATE TABLE `t_user_%d` (\n" +
+    public  static  final String SQL="CREATE TABLE `t_group_%d` (\n" +
             "  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "  `username` varchar(256) DEFAULT NULL COMMENT '用户名',\n" +
-            "  `password` varchar(512) DEFAULT NULL COMMENT '密码',\n" +
-            "  `real_name` varchar(256) DEFAULT NULL COMMENT '真实姓名',\n" +
-            "  `phone` varchar(128) DEFAULT NULL COMMENT '手机号',\n" +
-            "  `mail` varchar(512) DEFAULT NULL COMMENT '邮箱',\n" +
-            "  `deletion_time` bigint DEFAULT NULL COMMENT '注销时间戳',\n" +
+            "  `gid` varchar(32) DEFAULT NULL COMMENT '分组标识',\n" +
+            "  `name` varchar(64) DEFAULT NULL COMMENT '分组名称',\n" +
+            "  `username` varchar(256) DEFAULT NULL COMMENT '创建分组用户名',\n" +
+            "  `sort_order` int DEFAULT NULL COMMENT '分组排序',\n" +
             "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
             "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
-            "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识符 0：未删除 1：已删除',\n" +
+            "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
             "  PRIMARY KEY (`id`),\n" +
-            "  UNIQUE KEY `id_UNIQUE` (`id`),\n" +
-            "  UNIQUE KEY `username_UNIQUE` (`username`)\n" +
-            ") ENGINE=InnoDB AUTO_INCREMENT=1809112841173733378 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci KEY_BLOCK_SIZE=16";
+            "  UNIQUE KEY `idx_unique_username_gid` (`gid`,`username`) USING BTREE\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
             System.out.printf((SQL)+"%n",i);
