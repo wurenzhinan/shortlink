a11f0adef4b583a0cc3d4e625e275c28cfb2e201
wurenzhinan <wurenzhinan@163.com>
2024-07-11 10:50:59 +0800

    feature:优化用户注册重复提示报错

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -4,6 +4,7 @@ import com.nageoffer.shortlink.admin.common.convention.errorcode.IErrorCode;
 
 public enum UserErrorCodeEnum implements IErrorCode {
     USER_TOKEN_FAIL("A000200","用户Token验证失败"),
+    USER_EXIST("B000001","用户记录已存在"),
     USER_NULL("B000200","用户记录不存在"),
     USER_NAME_EXIST("B000201","用户名已存在"),
     USER_SAVE_ERROR("B000202","用户记录新增失败");
--- admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/UserServiceImpl.java
@@ -20,6 +20,7 @@ import org.redisson.api.RBloomFilter;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -80,9 +81,13 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY+requestParam.getUsername());
         try{
             if(lock.tryLock()){
-                int inserted=baseMapper.insert(BeanUtil.toBean(requestParam,UserDO.class));
-                if(inserted<1){
-                    throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+                try{
+                    int inserted=baseMapper.insert(BeanUtil.toBean(requestParam,UserDO.class));
+                    if(inserted<1) {
+                        throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+                    }
+                }catch(DuplicateKeyException exp){
+                    throw new ClientException(UserErrorCodeEnum.USER_EXIST);
                 }
                 userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
                 return;
