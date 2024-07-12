c7b2bddd35dbd688df405ef5795a923d6a6a0695
wurenzhinan <wurenzhinan@163.com>
2024-07-12 11:05:48 +0800

    refactor:短链接预热

--- project/src/main/java/com/nageoffer/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/nageoffer/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -38,6 +38,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -100,7 +101,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
         }
         stringRedisTemplate.opsForValue()
                 .set(
-                        fullShortUrl,
+                        String.format(GOTO_SHORT_LINK_KEY,fullShortUrl),
                         requestParam.getOriginUrl(),
                         LinkUtil.getLinkCacheValidTime(requestParam.getValidDate()),TimeUnit.MILLISECONDS);
         shortUriCreateCachePenetrationBloomFilter.add(fullShortUrl);
@@ -255,7 +256,16 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                     .eq(ShortLinkDO::getEnableStatus, 0);
             ShortLinkDO shortLinkDO=baseMapper.selectOne(queryWrapper);
             if(shortLinkDO!=null){
-                stringRedisTemplate.opsForValue().set(String.format(LOCK_GOTO_SHORT_LINK_KEY, fullShortUrl),shortLinkDO.getOriginUrl());
+                if(shortLinkDO.getValidDate()!=null&&shortLinkDO.getValidDate().before(new Date())){
+                    //如果缓存有效期已经失效了，就当作没有DO一样处理
+                    stringRedisTemplate.opsForValue().set(String.format(GOTO_IS_NULL_SHORT_LINK_KEY,fullShortUrl),"-",30, TimeUnit.MINUTES);
+                    return;
+                }
+                stringRedisTemplate.opsForValue()
+                        .set(
+                                String.format(GOTO_SHORT_LINK_KEY,fullShortUrl),
+                                shortLinkDO.getOriginUrl(),
+                                LinkUtil.getLinkCacheValidTime(shortLinkDO.getValidDate()),TimeUnit.MILLISECONDS);
                 ((HttpServletResponse) response).sendRedirect(shortLinkDO.getOriginUrl());
             }
         }finally {
