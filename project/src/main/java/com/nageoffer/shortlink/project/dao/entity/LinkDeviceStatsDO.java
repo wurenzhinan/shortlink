46ddb147345bf458c166421f1d3a789f81d361aa
wurenzhinan <wurenzhinan@163.com>
2024-07-15 21:19:35 +0800

    feature:开发短链接监控之访问设备功能

---
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/entity/LinkDeviceStatsDO.java
@@ -0,0 +1,52 @@
+package com.nageoffer.shortlink.project.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.nageoffer.shortlink.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 访问设备统计访问实体
+ * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
+ */
+@Data
+@TableName("t_link_device_stats")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LinkDeviceStatsDO extends BaseDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 访问量
+     */
+    private Integer cnt;
+
+    /**
+     * 访问设备
+     */
+    private String device;
+}
